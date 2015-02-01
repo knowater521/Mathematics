f2123345561e1f47f7fbe1d5acd5a9e3c42b84e1
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-02-01 23:00:47 +0900

    カプセル化&イテレータ改良

--- archimedes_pi_approximation.py
+++ archimedes_pi_approximation.py
@@ -12,63 +12,70 @@
 # n = 6, 12, 24, 48, 96, ...のときのl, mを計算することで
 # 円周率πの近似値を求めることができる。
 
+from copy import copy
 from decimal import Decimal, getcontext
 
 
 class PiApproximation():
+    __slots__ = ["__n", "__p"]
+
     def __init__(self, prec=28):
         # 計算精度
         getcontext().prec = prec
 
         # 正多角形の辺の数
-        self.n = 6
+        self.__n = 6
 
         # 円の中心から内接正n角形の1辺へ下ろした垂線の長さ
         # 半径1の円の内接正6角形の場合、sqrt(3)/2
-        self.p = Decimal(3).sqrt() / 2
+        self.__p = Decimal(3).sqrt() / 2
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.n *= 2
-        self.p = self.q()
-        return self
+        curr = copy(self)
+        self.__n *= 2
+        self.__p = self.__q()
+        return curr
 
     def print_result(self):
-        print("n = {0}のとき".format(self.n))
+        print("n = {0}のとき".format(self.__n))
         print("  {0} < π < {1}".format(self.l(), self.m()))
 
-    # 内接正n角形の1辺の長さの1/2
-    # = sqrt(1 - p^2)
-    def a(self):
-        return (1 - self.p ** 2).sqrt()
-
-    # 外接正n角形の1辺の長さの1/2
-    # = a/p
-    def b(self):
-        return self.a() / self.p
+    @property
+    def n(self):
+        return self.__n
 
     # 内接正n角形の周りの長さの1/2
     # = an
     def l(self):
-        return self.a() * self.n
+        return self.__a() * self.__n
 
     # 外接正n角形の周りの長さの1/2
     # = bn
     def m(self):
-        return self.b() * self.n
+        return self.__b() * self.__n
+
+    # 内接正n角形の1辺の長さの1/2
+    # = sqrt(1 - p^2)
+    def __a(self):
+        return (1 - self.__p ** 2).sqrt()
+
+    # 外接正n角形の1辺の長さの1/2
+    # = a/p
+    def __b(self):
+        return self.__a() / self.__p
 
     # 円の中心から内接正2n角形の1辺へ下ろした垂線の長さ
     # = sqrt((1 + p)/2)
-    def q(self):
-        return ((1 + self.p) / 2).sqrt()
+    def __q(self):
+        return ((1 + self.__p) / 2).sqrt()
 
 
 if __name__ == "__main__":
     # 利用例
     pa = PiApproximation(30)
-    pa.print_result()
     for pi in pa:
         pi.print_result()
         if pi.n > 10000000:
