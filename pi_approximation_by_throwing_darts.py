010515f32a3eae37b31202e67c45541b8b6a5404
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-11-17 23:01:17 +0900

    remove an unnecessary argument

--- pi_approximation_by_throwing_darts.py
+++ pi_approximation_by_throwing_darts.py
@@ -13,12 +13,12 @@ def pi_approx(sampling):
     def dart():
         x, y = random(), random()
         return 1 if x ** 2 + y ** 2 < 1 else 0
-    count = sum(dart() for n in range(1, sampling))
+    count = sum(dart() for n in range(sampling))
     return count / sampling * 4
 
 
 if __name__ == '__main__':
-    sampling = 10000
+    sampling = 1000000
     if len(sys.argv) == 2:
         sampling = int(sys.argv[1])
     print('π ≒ {0}'.format(pi_approx(sampling)))
