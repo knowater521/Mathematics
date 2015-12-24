b408445fe0d6074c547d98fadb4af27cf33d6675
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2015-12-24 13:32:01 +0900

    remove an unnecessary blank

--- PiApproximationByThrowingDarts.kt
+++ PiApproximationByThrowingDarts.kt
@@ -4,7 +4,7 @@ import java.lang.Math.random
 class PiApproximationByThrowingDarts {
   companion object {
     fun piApprox(sampling: Int): Double {
-      val dart =  { n: Int ->
+      val dart = { n: Int ->
         val x = random()
         val y = random()
         if (pow(x, 2.0) + pow(y, 2.0) < 1) 1 else 0
