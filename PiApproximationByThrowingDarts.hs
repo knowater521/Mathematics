59f8e2d9769e48f379563b2ee4248a0571a32640
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2015-12-24 09:48:32 +0900

    simplify the implementation

--- PiApproximationByThrowingDarts.hs
+++ PiApproximationByThrowingDarts.hs
@@ -3,17 +3,15 @@ module PiApproximationByThrowingDarts where
 import System.Environment (getArgs)
 import System.Random (randomRIO)
 
-piApprox :: (Fractional a, Enum a) => a -> IO a
+piApprox :: Double -> IO Double
 piApprox sampling = do
   count <- mapM dart [1..sampling]
-  return $ fromIntegral (sum count) / sampling * 4
+  return $ sum count / sampling * 4
     where
       dart _ = do
         x <- randomRIO (0, 1) :: IO Double
         y <- randomRIO (0, 1) :: IO Double
-        if x ^ 2 + y ^ 2 < 1
-          then return 1
-          else  return 0
+        return $ if x ^ 2 + y ^ 2 < 1 then 1 else 0
 
 main :: IO ()
 main = do
@@ -21,4 +19,5 @@ main = do
   let sampling = if length args == 1
                    then read $ head args :: Double
                    else 1000000
-  piApprox sampling >>= print
+  result <- piApprox sampling
+  putStrLn $ "π ≒ " ++ show result
