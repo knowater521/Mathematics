fce5a3c3c0c2c00da5e969a2fae73c9e0f3ddfec
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-12-27 00:57:58 +0900

    use Pair for multiple assignment

--- PiApproximationByThrowingDarts.kt
+++ PiApproximationByThrowingDarts.kt
@@ -5,8 +5,7 @@ class PiApproximationByThrowingDarts {
   companion object {
     fun piApprox(sampling: Int): Double {
       val dart = { n: Int ->
-        val x = random()
-        val y = random()
+        val (x, y) = random() to random()
         if (pow(x, 2.0) + pow(y, 2.0) < 1) 1 else 0
       }
       val count = (1..sampling).map(dart).fold(0) { a, b -> a + b }
@@ -16,6 +15,6 @@ class PiApproximationByThrowingDarts {
 }
 
 fun main(args: Array<String>): Unit {
-  val sampling = if (args.size() == 1) args[0].toInt() else 1000000
+  val sampling = if (args.size == 1) args[0].toInt() else 1000000
   println("π ≒ ${PiApproximationByThrowingDarts.piApprox(sampling)}")
 }
