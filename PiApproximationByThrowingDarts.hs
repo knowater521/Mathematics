704b1a506fb2c2e533857ae7ea3efaa09886159b
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-12-27 17:12:11 +0900

    adjust indentation level

--- PiApproximationByThrowingDarts.hs
+++ PiApproximationByThrowingDarts.hs
@@ -7,11 +7,11 @@ piApprox :: Double -> IO Double
 piApprox sampling = do
   count <- mapM dart [1..sampling]
   return $ sum count / sampling * 4
-    where
-      dart _ = do
-        x <- randomRIO (0, 1) :: IO Double
-        y <- randomRIO (0, 1) :: IO Double
-        return $ if x ^ 2 + y ^ 2 < 1 then 1 else 0
+  where
+    dart _ = do
+      x <- randomRIO (0, 1) :: IO Double
+      y <- randomRIO (0, 1) :: IO Double
+      return $ if x ^ 2 + y ^ 2 < 1 then 1 else 0
 
 main :: IO ()
 main = do
